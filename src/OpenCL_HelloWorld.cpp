3cf674f14ec3dd523399df600396afcefef3549e
Karoly2 Robert Nehez <karoly.nehez.ext@siemens.com>
2023-06-20 20:46:16 +0200

    compiler errors

--- src/OpenCL_HelloWorld.cpp
+++ src/OpenCL_HelloWorld.cpp
@@ -1,3 +1,4 @@
+#define __CL_ENABLE_EXCEPTIONS
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -22,7 +23,8 @@ static std::string findFile(std::string fileName)
     return fileName;
 }
 
-int main() {
+int main()
+{
     // Vektor mérete
     const int size = 10;
 
@@ -34,10 +36,11 @@ int main() {
     cl::Platform::get(&platforms);
     cl::Context context(CL_DEVICE_TYPE_GPU);
     std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
-    
+
     std::ifstream kernelSource(findFile("kernel.cl"));
-    
-    if(kernelSource.fail()){
+
+    if (kernelSource.fail())
+    {
         std::cout << "Current path is " << std::filesystem::current_path() << '\n';
         std::cout << "kernel.cl: file not found\n";
         return 1;
@@ -48,7 +51,31 @@ int main() {
     sources.push_back(sourceCode);
 
     cl::Program program(context, sources);
-    program.build(devices);
+    
+    try
+    {
+        program.build(devices);
+    }
+    catch (cl::Error &e)
+    {
+        if (e.err() == CL_BUILD_PROGRAM_FAILURE)
+        {
+            for (cl::Device dev : devices)
+            {
+                // Check the build status
+                cl_build_status status = program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(dev);
+                if (status != CL_BUILD_ERROR)
+                    continue;
+
+                // Get the build log
+                std::string name = dev.getInfo<CL_DEVICE_NAME>();
+                std::string buildlog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dev);
+                std::cerr << "Build log for " << name << ":" << std::endl
+                          << buildlog << std::endl;
+            }
+            return -1;
+        }
+    }
 
     // OpenCL kernel létrehozása
     cl::Kernel kernel(program, "vectorAdd");
@@ -64,13 +91,14 @@ int main() {
     cl::CommandQueue queue(context, devices[0]);
 
     queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(size));
-    
+
     // Eredmény vektor másolása v1-be a következő iterációhoz
     queue.enqueueReadBuffer(bufferV1, CL_TRUE, 0, sizeof(int) * size, v1.data());
 
     // Eredmény kiíratása
     std::cout << "v1: ";
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         std::cout << v1[i] << " ";
     }
     std::cout << std::endl;
