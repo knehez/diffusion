e0e96d26b22a507c6725418c21afa26378e33fe6
Karoly2 Robert Nehez <karoly.nehez.ext@siemens.com>
2023-06-20 20:57:10 +0200

    code review

--- src/OpenCL_HelloWorld.cpp
+++ src/OpenCL_HelloWorld.cpp
@@ -13,29 +13,48 @@ static std::string findFile(std::string fileName)
     {
         if (dirEntry.is_regular_file())
         {
-            std::filesystem::path file = dirEntry.path();
+            auto file = dirEntry.path();
             if (file.filename().string() == fileName)
             {
                 return file.string();
             }
         }
     }
+    // file not found, return the original name
     return fileName;
+}
+
+bool printError(const std::vector<cl::Device>& devices, const cl::Program& program, const cl::Error& e)
+{
+	if (e.err() == CL_BUILD_PROGRAM_FAILURE)
+	{
+		for (cl::Device dev : devices)
+		{
+			// Check the build status
+			const cl_build_status status = program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(dev);
+			if (status != CL_BUILD_ERROR)
+				continue;
+
+			// Get the build log
+			const auto name = dev.getInfo<CL_DEVICE_NAME>();
+			const auto buildLog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dev);
+			std::cerr << "Build log for " << name << ":" << std::endl
+				<< buildLog << std::endl;
+		}
+	}
+	return false;
 }
 
 int main()
 {
-    // Vektor mérete
     const int size = 10;
 
-    // Vektorok inicializálása
     std::vector<int> v1(size, 0);
 
-    // OpenCL inicializálása
     std::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
-    cl::Context context(CL_DEVICE_TYPE_GPU);
-    std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+    const cl::Context context(CL_DEVICE_TYPE_GPU);
+    const auto devices = context.getInfo<CL_CONTEXT_DEVICES>();
 
     std::ifstream kernelSource(findFile("kernel.cl"));
 
@@ -45,12 +64,12 @@ int main()
         std::cout << "kernel.cl: file not found\n";
         return 1;
     }
-    std::string sourceCode(std::istreambuf_iterator<char>{kernelSource}, {});
+    const std::string sourceCode(std::istreambuf_iterator<char>{kernelSource}, {});
 
     cl::Program::Sources sources;
     sources.push_back(sourceCode);
 
-    cl::Program program(context, sources);
+    const cl::Program program(context, sources);
     
     try
     {
@@ -58,44 +77,21 @@ int main()
     }
     catch (cl::Error &e)
     {
-        if (e.err() == CL_BUILD_PROGRAM_FAILURE)
-        {
-            for (cl::Device dev : devices)
-            {
-                // Check the build status
-                cl_build_status status = program.getBuildInfo<CL_PROGRAM_BUILD_STATUS>(dev);
-                if (status != CL_BUILD_ERROR)
-                    continue;
-
-                // Get the build log
-                std::string name = dev.getInfo<CL_DEVICE_NAME>();
-                std::string buildlog = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(dev);
-                std::cerr << "Build log for " << name << ":" << std::endl
-                          << buildlog << std::endl;
-            }
-            return -1;
-        }
+	    printError(devices, program, e);
+        return -1;
     }
 
-    // OpenCL kernel létrehozása
+
     cl::Kernel kernel(program, "vectorAdd");
+    const cl::Buffer bufferV1(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, sizeof(int) * size, v1.data());
 
-    // OpenCL buffer-ek létrehozása és másolása
-    cl::Buffer bufferV1(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
-                        sizeof(int) * size, v1.data());
-
-    // Kernel paraméterek beállítása
     kernel.setArg(0, bufferV1);
 
-    // OpenCL parancssor létrehozása
-    cl::CommandQueue queue(context, devices[0]);
+    const cl::CommandQueue queue(context, devices[0]);
 
     queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(size));
-
-    // Eredmény vektor másolása v1-be a következő iterációhoz
     queue.enqueueReadBuffer(bufferV1, CL_TRUE, 0, sizeof(int) * size, v1.data());
 
-    // Eredmény kiíratása
     std::cout << "v1: ";
     for (int i = 0; i < size; i++)
     {
